a9f7654a805603d932f78b00a0dc9f0b2f54bff1
kali linux <dmoharana4@gmail.com>
2018-07-21 11:47:14 +0530

    Commiting all the files upto date till jul 21 2018

---
+++ Practise.js
@@ -0,0 +1,79 @@
+// is execution context created while there is no content in javascript file ???
+
+
+// function Motor(name,type,engineCapacity,wheels,company) {
+//   this.name = name ;
+//   this.type = type ;
+//   this.engineCapacity = engineCapacity ;
+//   this.wheels = wheels ;
+//   this.company = company ;
+// }
+// var motor = new Motor('KTM 390' , 'sports' , 390 , 2 , 'KTM' );
+// console.log(motor);
+//
+// var motor2 = new Motor('Pulsar 150' , 'sports' , 150 , 2 , 'Bajaj')
+// console.log(motor2);
+//
+// function Animal(name , type , legs , ) {
+//   this.name = name ;
+//   this.type = type;
+//   this.legs = legs ;
+// }
+// var animal = new Animal ("Elephant" , "Herbivores" , 4) ;
+// var animal2 = new Animal ("Tiger" , "arnivores" , 4);
+// var animal3 = new Animal ("Human" , "Omnivores" , 2);
+//
+// console.log(animal);
+// console.log(animal2);
+// console.log(animal3);
+
+// Converting to boolean using !! operator
+
+
+
+// var a = 5 ;
+// if (c > a) {
+//   var c = 10;
+//   console.log(a + " is greater than " + c);
+// }
+// else {
+//   console.log(c + " is greater than " + a);
+// }
+
+
+
+// This is also called Hoistng
+// console.log(a);
+// var a ;
+
+
+// Both r same
+// if (conected) {
+//     login();
+// }
+//
+// conected && login();
+
+
+// Getting the last item in the array
+// var array = [1, 2, 3, 4, 5, 6];
+// console.log(array.slice(-1)); // [6]
+// console.log(array.slice(-2)); // [5,6]
+// console.log(array.slice(-3)); // [4,5,6]
+
+
+
+// Array truncation (To limit the array length)
+// var array = [1, 2, 3, 4, 5, 6];
+// console.log(array.length); // 6
+// array.length = 3;
+// console.log(array.length); // 3
+// console.log(array); // [1,2,3]
+
+
+
+// Shuffling array’s elements
+// var list = [1, 2, 3];
+// console.log(list.sort(function() {
+//     return Math.random() - 0.5
+// })); // [2,1,3]
---
+++ if_else.js
@@ -0,0 +1,17 @@
+// Change Text TO UpperCase.............................
+
+// var input = document.createElement("input");
+// input.setAttribute("name",'email');
+// input.setAttribute("type" , 'text');
+// var button = document.createElement("button");
+// button.innerHTML = "change Case";
+// button.setAttribute("type" , 'button');
+// button.setAttribute("onclick" , 'changeCase()');
+// document.body.appendChild(input);
+// document.body.appendChild(button);
+// function changeCase() {
+//   var email = document.getElementsByName('email')[0].value;
+//   console.log(email);
+//   email = email.toUpperCase();
+//   document.getElementsByName('email')[0].value = email ;
+// }
